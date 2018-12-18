8bd5a0e59b50e9a242566fa122a0f13087d67127
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2018-12-18 15:51:17 +0100

    Solved day18

--- day18/day18.py
+++ day18/day18.py
@@ -1,4 +1,4 @@
-import copy
+import functools
 
 
 class Simulation:
@@ -7,31 +7,38 @@ class Simulation:
         self.map = load_initial(fname)
         self.height = len(self.map)
         self.width = len(self.map[0]) if self.height > 0 else 0
+        self.old_map = [[0 for _ in range(self.width)] for _ in range(self.height)]
 
     def step(self):
-        old_map = copy.deepcopy(self.map)
-        for y, row in enumerate(old_map):
+        self.old_map, self.map = self.map, self.old_map  # double buffering
+        for y, row in enumerate(self.old_map):
             for x, current in enumerate(row):
-                open_ground, trees, lumbyard = self.count(old_map, x, y)
+                neighborhood = self.neighborhood(x, y)
+                trees = sum(1 for (xx, yy) in neighborhood if self.old_map[yy][xx] == '|')
+                lumberyard = sum(1 for (xx, yy) in neighborhood if self.old_map[yy][xx] == '#')
                 self.map[y][x] = current
                 if current == '.' and trees >= 3:
                     self.map[y][x] = '|'
-                elif current == '|' and lumbyard >= 3:
+                elif current == '|' and lumberyard >= 3:
                     self.map[y][x] = '#'
-                elif current == '#' and (lumbyard == 0 or trees == 0):
+                elif current == '#' and (lumberyard == 0 or trees == 0):
                     self.map[y][x] = '.'
 
     def run(self, steps=1):
-        viewed = set()
-        for _ in range(steps):
-            frozen_map = freeze(self.map)
-            if frozen_map in viewed:
-                print("Found !!!!!!")
-                return
+        last_seen = {}
+        step = 0
+        while step < steps:
+            hashable = self.show()
+            if hashable in last_seen:
+                remaining = (steps - step) % (step - last_seen[hashable])
+                step = steps - remaining
+                last_seen = {}
             else:
-                viewed.add(frozen_map)
+                last_seen[hashable] = step
             self.step()
+            step += 1
 
+    @functools.lru_cache(maxsize=None)
     def neighborhood(self, x, y):
         neighbors = []
         for dx in [-1, 0, +1]:
@@ -45,36 +52,22 @@ class Simulation:
                 neighbors.append((xx, yy))
         return neighbors
 
-    def count(self, map_, x, y):
-        neighborhood = self.neighborhood(x, y)
-        open_ground = sum(1 for (xx, yy) in neighborhood if map_[yy][xx] == '.')
-        trees = sum(1 for (xx, yy) in neighborhood if map_[yy][xx] == '|')
-        lumberyard = sum(1 for (xx, yy) in neighborhood if map_[yy][xx] == '#')
-        return open_ground, trees, lumberyard
-
     def show(self):
         return '\n'.join(''.join(c for c in row) for row in self.map)
 
-def freeze(map_):
-    return tuple(c for row in map_ for c in row)
 
 def load_initial(fname):
     with open(fname, 'r') as file:
         return [[c for c in row.strip()] for row in file]
 
-def part1(fname):
-    simulation = Simulation(fname)
-    simulation.run(10)
-    trees = sum(1 for row in simulation.map for c in row if c == '|')
-    lumber = sum(1 for row in simulation.map for c in row if c == '#')
-    return trees * lumber
 
-def part2(fname, steps):
+def part(fname, steps):
     simulation = Simulation(fname)
     simulation.run(steps)
     trees = sum(1 for row in simulation.map for c in row if c == '|')
     lumber = sum(1 for row in simulation.map for c in row if c == '#')
     return trees * lumber
+
 
 def test_load_initial():
     expected = list(map(list, ['.#.#...|#.',
@@ -126,10 +119,11 @@ def test_ten_steps():
     simulation.run(10)
     assert expected == simulation.show()
 
+
 def test_part1():
-    assert 1147 == part1('test_input.txt')
+    assert 1147 == part('test_input.txt', 10)
+
 
 if __name__ == '__main__':
-    print('Part1', part1('input.txt'))
-    #print('Part1', part2('input.txt', 1000)) # 207640
-    print('Part1', part2('input.txt', 1100)) # 207640
+    print('Part1', part('input.txt', 10))
+    print('Part2', part('input.txt', 1000000000))
