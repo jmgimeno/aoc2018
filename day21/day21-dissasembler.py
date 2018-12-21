9f4b239d0565db57ea59ede394b15e51c6a01e43
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2018-12-21 13:07:34 +0100

    Solved day21

---
+++ day21/day21-dissasembler.py
@@ -0,0 +1,69 @@
+import os
+
+OPS = {'addi': 'r[{c}] = r[{a}] + {b}',
+       'addr': 'r[{c}] = r[{a}] + r[{b}]',
+       'muli': 'r[{c}] = r[{a}] * 0x{b:08x}',
+       'mulr': 'r[{c}] = r[{a}] * r[{b}]',
+       'seti': 'r[{c}] = 0x{a:08x}',
+       'setr': 'r[{c}] = r[{a}]',
+       'eqrr': 'r[{c}] = 1 if r[{a}] == r[{b}] else 0',
+       'eqri': 'r[{c}] = 1 if r[{a}] == 0x{b:08x} else 0',
+       'gtrr': 'r[{c}] = 1 if r[{a}] > r[{b}] else 0',
+       'gtir': 'r[{c}] = 1 if 0x{a:08x} > r[{b}] else 0',
+       'bani': 'r[{c}] = r[{a}] & 0x{b:08x}',
+       'bori': 'r[{c}] = r[{a}] | 0x{b:08x}'
+       }
+
+
+def disassemble_line(line, lineno, ipreg):
+    opname = line[:4]
+    a, b, c = map(int, line[4:].split())
+    if opname == 'addi' and c == ipreg and a == ipreg:
+        return 'jump #%d' % (b+lineno+1,)
+    elif opname == 'addr' and c == ipreg and a == ipreg:
+        return 'jump r[%d] + %d' % (b, lineno+1)
+    elif opname == 'addr' and c == ipreg and b == ipreg:
+        return 'jump r[%d] + %d' % (a, lineno+1)
+    elif opname == 'seti' and c == ipreg:
+        return 'jump #%d' % (a+1,)
+    elif opname == 'mulr' and a == ipreg and b == ipreg and c == ipreg:
+        return 'jump #%d' %(lineno * lineno + 1,)
+    elif opname == 'addr' and b == ipreg:
+        return 'r[%d] = r[%d] + %d' %(c, a, lineno)
+    elif opname == 'addr' and a == ipreg:
+        return 'r[%d] = r[%d] + %d' % (c, b, lineno)
+    elif opname == 'mulr' and b == ipreg:
+        return 'r[%d] = r[%d] * %d' %(c, a, lineno)
+    elif opname == 'mulr' and a == ipreg:
+        return 'r[%d] = r[%d] * %d' % (c, b, lineno)
+    elif opname == 'setr' and a == ipreg:
+        return 'r[%d] = %d' % (c, lineno)
+    else:
+        return OPS[opname].format(a=a, b=b, c=c)
+
+
+def disassemble_lines(lines, ipreg):
+    cases = []
+    for lineno, line in enumerate(lines):
+        cases.append("%2d# %s" % (lineno, disassemble_line(line, lineno, ipreg)))
+    return '\n'.join(cases)
+
+
+def disassemble_file(fname, registers):
+    with open(fname, 'r') as program:
+        ipreg = int(program.readline()[4:])
+        registers = 'r = ' + str(registers) + '\n'
+        instructions = disassemble_lines((line.strip() for line in program), ipreg)
+        return registers, instructions
+
+
+def part1(fname):
+    name, _ = os.path.splitext(fname)
+    with open(name + '_disassembled.txt', 'w') as compiled:
+        registers, instructions = disassemble_file(fname, [1, 0, 0, 0, 0, 0])
+        compiled.write(registers)
+        compiled.write(instructions)
+
+
+if __name__ == "__main__":
+    part1('input.txt')
---
+++ day21/part2.cc
@@ -0,0 +1,132 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int regs[] = {0, 0, 0, 0, 0, 0};
+
+void addr(int a, int b, int c) {
+  regs[c] = regs[a] + regs[b];
+}
+
+void addi(int a, int b, int c) {
+  regs[c] = regs[a] + b;
+}
+
+void mulr(int a, int b, int c) {
+  regs[c] = regs[a] * regs[b];
+}
+
+void muli(int a, int b, int c) {
+  regs[c] = regs[a] * b;
+}
+
+void banr(int a, int b, int c) {
+  regs[c] = regs[a] & regs[b];
+}
+
+void bani(int a, int b, int c) {
+  regs[c] = regs[a] & b;
+}
+
+void borr(int a, int b, int c) {
+  regs[c] = regs[a] | regs[b];
+}
+
+void bori(int a, int b, int c) {
+  regs[c] = regs[a] | b;
+}
+
+void setr(int a, int b, int c) {
+  regs[c] = regs[a];
+}
+
+void seti(int a, int b, int c) {
+  regs[c] = a;
+}
+
+void gtir(int a, int b, int c) {
+  regs[c] = (a > regs[b]) ? 1 : 0;
+}
+
+void gtri(int a, int b, int c) {
+  regs[c] = (regs[a] > b) ? 1 : 0;
+}
+
+void gtrr(int a, int b, int c) {
+  regs[c] = (regs[a] > regs[b]) ? 1 : 0;
+}
+
+void eqir(int a, int b, int c) {
+  regs[c] = (a == regs[b]) ? 1 : 0;
+}
+
+void eqri(int a, int b, int c) {
+  regs[c] = (regs[a] == b) ? 1 : 0;
+}
+
+void eqrr(int a, int b, int c) {
+  regs[c] = (regs[a] == regs[b]) ? 1 : 0;
+}
+
+bool comparator(const pair<int, long>& p1, const pair<int, long>& p2) {
+    return p1.second < p2.second;
+}
+
+int main() {
+  long counter = 0;
+  map<int, long> seen;
+
+  while (1) {
+    switch(regs[2]) {
+      case 0: seti(123, 0, 5); break;
+      case 1: bani(5, 456, 5); break;
+      case 2: eqri(5, 72, 5); break;
+      case 3: addr(5, 2, 2); break;
+      case 4: seti(0, 0, 2); break;
+      case 5: seti(0, 4, 5); break;
+      case 6: bori(5, 65536, 1); break;
+      case 7: seti(10678677, 3, 5); break;
+      case 8: bani(1, 255, 4); break;
+      case 9: addr(5, 4, 5); break;
+      case 10: bani(5, 16777215, 5); break;
+      case 11: muli(5, 65899, 5); break;
+      case 12: bani(5, 16777215, 5); break;
+      case 13: gtir(256, 1, 4); break;
+      case 14: addr(4, 2, 2); break;
+      case 15: addi(2, 1, 2); break;
+      case 16: seti(27, 5, 2); break;
+      case 17: seti(0, 6, 4); break;
+      case 18: addi(4, 1, 3); break;
+      case 19: muli(3, 256, 3); break;
+      case 20: gtrr(3, 1, 3); break;
+      case 21: addr(3, 2, 2); break;
+      case 22: addi(2, 1, 2); break;
+      case 23: seti(25, 4, 2); break;
+      case 24: addi(4, 1, 4); break;
+      case 25: seti(17, 6, 2); break;
+      case 26: setr(4, 6, 1); break;
+      case 27: seti(7, 5, 2); break;
+      case 28: eqrr(5, 0, 4); if (seen.find(regs[5]) != seen.end()) {
+                                map<int, long>::iterator maxkey = max_element(seen.begin(),
+                                                          seen.end(),
+                                                          comparator);
+                                cout << "Part2: " << maxkey->first << endl;
+                                return 0;
+                              } else {
+                                seen[regs[5]] = counter;
+                              }
+                              break;
+      case 29: addr(4, 2, 2); break;
+      case 30: seti(5, 4, 2); break;
+      default: cout << "Finished " << regs[0] << " " << counter<< endl; return 0;
+    }
+
+    if (counter % 1000000 == 0)
+        cout << "Counter: " << counter << " Seen size: " << seen.size() << endl;
+
+    regs[2] += 1;
+    counter += 1;
+  }
+}
