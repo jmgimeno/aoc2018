3a287a8d1d7056a40cb3d2d1019b9b08c576fa67
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2018-12-21 20:57:16 +0100

    Solved day20

--- day20/day20.py
+++ day20/day20.py
@@ -1,72 +1,120 @@
-import collections
+from collections import defaultdict, namedtuple
 
+Leg = namedtuple('Leg', 'start begin end cont_start cont_end')
+
+def mkleg(start, begin, end, cont_start=None, cont_end=None):
+    return Leg(start, begin, end, cont_start, cont_end)
 
 class Expedition:
 
     def __init__(self, regexp):
-        self.walkers = []
-        self.explored = collections.defaultdict(lambda: float('inf'))
+        self.regexp = regexp
+        self.distances = defaultdict(lambda: float('inf'))
+        self.doors = set()
+        self.legs = {mkleg((1, 1), 0, len(self.regexp))}
 
     def explore(self):
-        walker = Walker(0,0, self.explored)
-        self.walkers.append(walker)
-        # To be continued ...
+        assert self.regexp[0] == '^' and self.regexp[-1] == '$'
+        counter = 0
+        while len(self.legs) > 0:
+            next_leg = self.legs.pop()
+            self.explore_leg(next_leg)
+            counter += 1
 
-class Walker:
+    def explore_leg(self, current_leg):
+        (x, y) = current_leg.start
+        idx = current_leg.begin
+        finish = False
+        while idx < current_leg.end and not finish:
+            old_pos = (x, y)
+            current = self.regexp[idx]
+            if current == '^':
+                self.distances[(x, y)] = 0
+            elif current == 'N':
+                y -= 2
+            elif current == 'E':
+                x += 2
+            elif current == 'S':
+                y += 2
+            elif current == 'W':
+                x -= 2
+            elif current == '$':
+                pass
+            elif current == '(':
+                matching = self.find_matching(idx)
+                options = self.find_options(idx, matching)
+                for (option_begin, option_end) in options:
+                    assert option_begin <= option_end
+                    if option_begin < option_end:
+                        leg = mkleg((x, y), option_begin, option_end, matching, current_leg.end)
+                    else:
+                        leg = mkleg((x, y), matching, current_leg.end)
+                    self.legs.add(leg)
+                finish = True
+            elif current == ')':
+                pass
+            else:
+                raise Exception('unexpected character ', current)
+            self.distances[(x, y)] = min(1 + self.distances[old_pos], self.distances[(x, y)])
+            idx += 1
+        if current_leg.cont_start and current_leg.cont_start < current_leg.cont_end:
+            leg = mkleg((x, y), current_leg.cont_start, current_leg.cont_end)
+            self.legs.add(leg)
 
-    def __init__(self, x=0, y=0, distances=None):
-        self.contexts = []
-        self.x = x
-        self.y = y
-        if not distances:
-            self.distances = collections.defaultdict(lambda: float('inf'))
-        else:
-            self.distances = distances
+    def find_matching(self, idx):
+        assert self.regexp[idx] == '('
+        opened = 0
+        while True:
+            idx += 1
+            current = self.regexp[idx]
+            if current == '(':
+                opened += 1
+            elif current == ')' and opened == 0:
+                return idx + 1
+            elif current == ')':
+                opened -= 1
+            elif current == '^':
+                raise Exception("regexp should be well constructed")
+
+    def find_options(self, begin, end):
+        assert self.regexp[begin] == '(' and self.regexp[end - 1] == ')'
+        options = []
+        opened = 0
+        option_start = begin + 1
+        for idx in range(begin + 1, end):
+            current = self.regexp[idx]
+            if current == '(':
+                opened += 1
+            elif current == ')':
+                opened -= 1
+            elif current == '|' and opened == 0:
+                options.append((option_start, idx))
+                option_start = idx + 1
+            idx += 1
+        options.append((option_start, end))
+        return options
 
 
-    def step(self, s):
-        current = self.x, self.y
-        if s == '^':
-            self.contexts.append(current)
-            self.distances[current] = 0
-        elif s == 'N':
-            self.y -= 1
-        elif s == 'E':
-            self.x += 1
-        elif s == 'S':
-            self.y += 1
-        elif s == 'W':
-            self.x -= 1
-        elif s == '(':
-            self.contexts.append((self.x, self.y))
-        elif s == '|':
-            self.x, self.y = self.contexts[-1]
-        elif s == ')':
-            self.contexts.pop()
-            self.x, self.y = self.contexts[-1]
-        elif s == '$':
-            self.contexts.pop()
-        else:
-            raise Exception('impossible %s' % s)
-        self.distances[(self.x, self.y)] = min(1 + self.distances[current], self.distances[(self.x, self.y)])
+def part1(regexp):
+    expedition = Expedition(regexp)
+    expedition.explore()
+    return max(expedition.distances.values())
 
-    def walk(self, steps):
-        for s in steps:
-            self.step(s)
-
-    def distance_to_farthest(self):
-        return max(self.distances.values())
-
-
-def part1(steps):
-    w = Walker()
-    w.walk(steps)
-    print(w.distances)
-    return w.distance_to_farthest()
-
+def part2(regexp):
+    expedition = Expedition(regexp)
+    expedition.explore()
+    return sum(1 for v in expedition.distances.values() if v >= 1000)
 
 def test_part1():
-    assert part1('^$') == 0
     assert part1('^WNE$') == 3
     assert part1('^ENWWW(NEEE|SSE(EE|N))$') == 10
-    # assert part1('^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$') == 18
+    assert part1('^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$') == 18
+    assert part1('^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$') == 23
+    assert part1('^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$') == 31
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as file:
+        contents = file.read().strip()
+        print("Part1: ", part1(contents))
+        print("Part2: ", part2(contents))
