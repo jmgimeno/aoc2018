de73387463bfdb278bd049c36de0b447e2704c28
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2018-12-23 20:35:06 +0100

    Wip on day15

--- day15/day15.py
+++ day15/day15.py
@@ -1,26 +1,44 @@
-from collections import namedtuple
-from pytest import fixture
+import functools
 
-Goblin = namedtuple('Goblin', 'x, y')
-Elf = namedtuple('Elf', 'x, y')
+import pytest
+
+
+class Unit:
+    ELF, GOBLIN = range(2)
+
+    def __init__(self, x, y, kind, hit_points=200, attack_power=3):
+        self.x = x
+        self.y = y
+        self.kind = kind
+        self.hit_points = hit_points
+        self.attack_power = attack_power
+        self.is_alive = True
+
+    def __eq__(self, other):
+        return (self.x == other.x
+                and self.y == other.y
+                and self.kind == other.kind)
+
+    def __repr__(self):
+        kind = ['Elf', 'Goblin'][self.kind]
+        return "%s(x=%d, y=%d)" % (kind, self.x, self.y)
+
+
+letter_to_kind = {'G': Unit.GOBLIN, 'E': Unit.ELF}
+kind_to_letter = {Unit.GOBLIN: 'G', Unit.ELF: 'E'}
 
 
 def parse_lines(lines):
-    units = set()
     cave = []
+    units = []
     for y, line in enumerate(lines):
         row = []
         for x, c in enumerate(line.strip()):
-            if c == '#':
-                row.append(False)
-            elif c == '.':
-                row.append(True)
-            elif c == 'G':
-                row.append(True)
-                units.add(Goblin(x=x, y=y))
-            elif c == 'E':
-                row.append(True)
-                units.add(Elf(x=x, y=y))
+            if c in ('#', '.'):
+                row.append(c)
+            elif c in ('G', 'E'):
+                units.append(Unit(x=x, y=y, kind=letter_to_kind[c]))
+                row.append(len(units) - 1)
             else:
                 raise Exception(
                     'impossible character %c at (%d, %d)' % (c, x, y))
@@ -34,76 +52,127 @@ class Simulation:
         self.cave, self.units = parse_lines(lines)
         self.occupied_positions = {(u.x, u.y) for u in self.units}
 
-    def turn_order(self):
-        return sorted(self.units, key=lambda unit: (unit.y, unit.x))
+    def __repr__(self):
+        repr_cave = []
+        for row in self.cave:
+            repr_row = []
+            for c in row:
+                if c in ('#', '.'):
+                    repr_row.append(c)
+                else:
+                    repr_row.append(kind_to_letter[self.units[c].kind])
+            repr_cave.append(''.join(repr_row))
+        return '\n'.join(repr_cave)
 
-    def possible_targets(self, unit):
-        return {target for target in self.units if type(target) != type(unit)}
+    @functools.lru_cache(maxsize=None)
+    def adjacent_to(self, x, y):
+        assert self.cave[y][x] != '#'
+        return [(xx, yy)
+                for (xx, yy) in [(x, y - 1), (x + 1, y), (x, y + 1), (x - 1, y)]
+                if self.cave[yy][xx] != '#']
 
-    def in_range(self, unit):
-        return {(x, y)
-                for target in self.possible_targets(unit)
-                for (x, y) in adjacent_to(target) - self.occupied_positions
-                if self.cave[y][x]}
+    def round(self):
+        sorted_units = sorted(self.units, key=lambda u: (u.y, u.x))
+        for unit in sorted_units:
+            if unit.is_alive:
+                self.turn(unit)
 
-    def can_reach(self, begin, end, visited=None):
-        if visited is None:
+    def turn(self, unit):
+        if not self.enemy_in_range(unit):
+            self.move_to_range(unit)
+        enemy = self.enemy_in_range(unit)
+        if enemy:
+            self.attack(unit, enemy)
+
+
+    def enemy_in_range(self, unit):
+        enemies = []
+        for (x, y) in self.adjacent_to(unit.x, unit.y):
+            c = self.cave[y][x]
+            if c != '.' and self.units[c].kind != unit.kind:
+                enemies.append(self.units[c])
+        return min(enemies, key=lambda u: (u.hit_points, u.y , u.x)) \
+                if len(enemies) > 0 else None
+
+    def next_pos(self, unit):
+        unit_pos = unit.x, unit.y
+        targets = self.find_targets(unit)
+        in_range = self.find_in_range(targets)
+        reachable = self.find_reachable(unit_pos, in_range)
+        nearest = self.find_nearest(unit_pos, reachable)
+        chosen = self.find_chosen(nearest)
+        return self.find_step(unit_pos, chosen)
+
+    def find_targets(self, unit):
+        return [target
+                for target in self.units
+                if target.kind != unit.kind and target.is_alive]
+
+    def find_in_range(self, targets):
+        return {pos
+                for target in targets
+                for pos in self.adjacent_to(target.x, target.y)}
+
+    def find_reachable(self, begin, in_range):
+        return {end for end in in_range if self.is_reachable(begin, end)}
+
+    def is_reachable(self, begin, end, visited=None):
+        if not visited:
             visited = set()
         if begin == end:
             return True
-        next_positions = {(x, y)
-                          for (x, y) in adjacent_to(begin) - self.occupied_positions - visited
-                          if self.cave[y][x]}
-        if len(next_positions) == 0:
-            return False
-        return any(self.can_reach(step, end, visited | {begin}) for step in next_positions)
+        next_positions = ((x, y)
+                          for (x, y) in self.adjacent_to(begin[0], begin[1])
+                          if (x, y) not in self.occupied_positions | visited)
+        return any(self.is_reachable(step, end, visited | {begin})
+                   for step in next_positions)
 
-    def reachable(self, unit):
-        return {position for position in self.in_range(unit) if self.can_reach((unit.x, unit.y), position)}
-
-    def distance(self, unit_or_tuple, end, visited=None):
+    def distance(self, begin, end, visited=None):
         if visited is None:
             visited = set()
-        if isinstance(unit_or_tuple, tuple):
-            begin = unit_or_tuple
-        else:
-            begin = unit_or_tuple.x, unit_or_tuple.y
         if begin == end:
             return 0
         next_positions = {(x, y)
-                          for (x, y) in adjacent_to(begin) - self.occupied_positions - visited
-                          if self.cave[y][x]}
+                          for (x, y) in self.adjacent_to(begin[0], begin[1])
+                          if (x, y) not in self.occupied_positions | visited}
         if len(next_positions) == 0:
             return float("+inf")
-        return 1 + min(self.distance(step, end, visited | {begin}) for step in next_positions)
+        return 1 + min(self.distance(step, end, visited | {begin})
+                       for step in next_positions)
 
-    def nearest(self, unit):
-        closests = set()
+    def find_nearest(self, begin, reachable):
+        nearest = set()
         min_distance = float("+inf")
-        for end in self.in_range(unit):
-            d = self.distance(unit, end)
+        for end in reachable:
+            d = self.distance(begin, end)
             if d < min_distance:
-                closests = {end}
+                nearest = {end}
                 min_distance = d
             elif d == min_distance:
-                closests.add(end)
-        return closests
+                nearest.add(end)
+        return nearest
 
-    def chosen(self, unit):
-        sorted_nearest = sorted(self.nearest(
-            unit), key=lambda pos: (pos[1], pos[0]))
-        return sorted_nearest[0] if len(sorted_nearest) > 0 else None
+    def find_chosen(self, nearest):
+        return min(nearest, key=lambda p: (p[1], p[0]))
+
+    def find_step(self, begin, chosen):
+        return min((neighbour for neighbour in self.adjacent_to(begin[0], begin[1])),
+                   key=lambda xy: (self.distance(xy, chosen), xy[1], xy[0]))
+
+    def move_to_range(self, unit):
+        x, y = self.next_pos(unit)
+        self.cave[y][x] = self.cave[unit.y][unit.y]
+        self.cave[unit.y][unit.x] = '.'
+        unit.x, unit.y = x, y
+
+    def attack(self, unit, enemy):
+        enemy.hit_points -= unit.attack_power
+        if enemy.hit_points <= 0:
+            enemy.is_alive = False
+            self.cave[enemy.y][enemy.x] = '.'
 
 
-def adjacent_to(unit_or_tuple):
-    if isinstance(unit_or_tuple, tuple):
-        x, y = unit_or_tuple
-    else:
-        x, y = unit_or_tuple.x, unit_or_tuple.y
-    return {(x, y-1), (x+1, y), (x, y+1), (x-1, y)}
-
-
-@fixture
+@pytest.fixture
 def simulation():
     lines = ["#######",
              "#E..G.#",
@@ -114,59 +183,79 @@ def simulation():
 
 
 def test_parse_lines(simulation):
-    expected_cave = [[False, False, False, False, False, False, False],
-                     [False, True,  True,  True,  True,  True,  False],
-                     [False, True,  True,  True,  False,  True,  False],
-                     [False, True,  True,  True,  False,  True,  False],
-                     [False, False, False, False, False, False, False]]
-    expected_units = {Elf(x=1, y=1), Goblin(x=4, y=1),
-                      Goblin(x=2, y=3), Goblin(x=5, y=3)}
-
+    expected_cave = [['#', '#', '#', '#', '#', '#', '#'],
+                     ['#', 0, '.', '.', 1, '.', '#'],
+                     ['#', '.', '.', '.', '#', '.', '#'],
+                     ['#', '.', 2, '.', '#', 3, '#'],
+                     ['#', '#', '#', '#', '#', '#', '#']]
+    expected_units = [Unit(x=1, y=1, kind=Unit.ELF),
+                      Unit(x=4, y=1, kind=Unit.GOBLIN),
+                      Unit(x=2, y=3, kind=Unit.GOBLIN),
+                      Unit(x=5, y=3, kind=Unit.GOBLIN)]
     assert expected_cave == simulation.cave
     assert expected_units == simulation.units
 
 
-def test_turn_order(simulation):
-    assert [Elf(x=1, y=1), Goblin(x=4, y=1),
-            Goblin(x=2, y=3), Goblin(x=5, y=3)] \
-        == simulation.turn_order()
+def test_repr(simulation):
+    expected = '''\
+#######
+#E..G.#
+#...#.#
+#.G.#G#
+#######'''
+    assert repr(simulation) == expected
 
 
-def test_in_range(simulation):
-    assert {(3, 1), (5, 1), (2, 2), (5, 2), (1, 3), (3, 3)} \
-        == simulation.in_range(Elf(x=1, y=1))
+def test_find_targets(simulation):
+    expected = [Unit(x=4, y=1, kind=Unit.GOBLIN),
+                Unit(x=2, y=3, kind=Unit.GOBLIN),
+                Unit(x=5, y=3, kind=Unit.GOBLIN)]
+    assert simulation.find_targets(Unit(x=1, y=1, kind=Unit.ELF)) == expected
 
 
-def test_can_reach(simulation):
-    assert simulation.can_reach((1, 1), (1, 1))
-    assert simulation.can_reach((1, 1), (3, 1))
-    assert simulation.can_reach((1, 1), (2, 2))
-    assert simulation.can_reach((1, 1), (1, 3))
-    assert simulation.can_reach((1, 1), (3, 3))
-    assert not simulation.can_reach((1, 1), (5, 1))
-    assert not simulation.can_reach((1, 1), (5, 2))
-    assert not simulation.can_reach((1, 1), (0, 0))
+def test_find_in_range(simulation):
+    expected = {(3, 1), (5, 1), (2, 2), (5, 2), (1, 3), (3, 3)}
+    unit = Unit(x=1, y=1, kind=Unit.ELF)
+    targets = simulation.find_targets(unit)
+    assert simulation.find_in_range(targets) == expected
 
 
-def test_reachable(simulation):
-    assert {(3, 1), (2, 2), (1, 3), (3, 3)} \
-        == simulation.reachable(Elf(x=1, y=1))
+def test_find_reachable(simulation):
+    expected = {(3, 1), (2, 2), (1, 3), (3, 3)}
+    unit = Unit(x=1, y=1, kind=Unit.ELF)
+    targets = simulation.find_targets(unit)
+    in_range = simulation.find_in_range(targets)
+    assert simulation.find_reachable((unit.x, unit.y), in_range) == expected
 
 
-def test_min_distance(simulation):
-    assert 0 == simulation.distance((1, 1), (1, 1))
-    assert 2 == simulation.distance((1, 1), (3, 1))
-    assert 2 == simulation.distance((1, 1), (2, 2))
-    assert 2 == simulation.distance((1, 1), (1, 3))
-    assert 4 == simulation.distance((1, 1), (3, 3))
-    assert float("+inf") == simulation.distance((1, 1), (5, 1))
-    assert float("+inf") == simulation.distance((1, 1), (5, 2))
-    assert float("+inf") == simulation.distance((1, 1), (0, 0))
+def test_find_reachable(simulation):
+    expected = {(3, 1), (2, 2), (1, 3)}
+    unit = Unit(x=1, y=1, kind=Unit.ELF)
+    targets = simulation.find_targets(unit)
+    in_range = simulation.find_in_range(targets)
+    reachable = simulation.find_reachable((unit.x, unit.y), in_range)
+    assert simulation.find_nearest((unit.x, unit.y), reachable) == expected
 
 
-def test_nearest(simulation):
-    assert {(3, 1), (2, 2), (1, 3)} == simulation.nearest(Elf(x=1, y=1))
+def test_find_chosen(simulation):
+    unit = Unit(x=1, y=1, kind=Unit.ELF)
+    targets = simulation.find_targets(unit)
+    in_range = simulation.find_in_range(targets)
+    reachable = simulation.find_reachable((unit.x, unit.y), in_range)
+    nearest = simulation.find_nearest((unit.x, unit.y), reachable)
+    assert simulation.find_chosen(nearest) == (3, 1)
 
 
-def test_chosen(simulation):
-    assert (3, 1) == simulation.chosen(Elf(x=1, y=1))
+def test_find_step(simulation):
+    unit = Unit(x=1, y=1, kind=Unit.ELF)
+    targets = simulation.find_targets(unit)
+    in_range = simulation.find_in_range(targets)
+    reachable = simulation.find_reachable((unit.x, unit.y), in_range)
+    nearest = simulation.find_nearest((unit.x, unit.y), reachable)
+    chosen = simulation.find_chosen(nearest)
+    assert simulation.find_step((unit.x, unit.y), chosen) == (2, 1)
+
+
+def test_next_pos(simulation):
+    unit = Unit(x=1, y=1, kind=Unit.ELF)
+    assert simulation.next_pos(unit) == (2, 1)
